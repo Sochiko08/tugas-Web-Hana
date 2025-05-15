6ae4f1c293888d8bbfbf45ad606ecf7a4dd068dc
Sochiko08 <sochikohanada@gmail.com>
2025-05-15 21:34:45 +0700

    Delete soal1-2.js

--- soal1-2.js
+++
@@ -1,9 +0,0 @@
-// Problem 1 - String Concatenation
-let firstName = 'Sochiko';
-let lastName = 'Hanada';
-let bornYear = 2006;
-
-let currentYear = 2025;
-let age = currentYear - bornYear;
-
-console.log('Hello, ' + firstName + ' ' + lastName + '! You are ' + age + ' years old.');
